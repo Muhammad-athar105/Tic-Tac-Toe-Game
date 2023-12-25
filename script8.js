a0a52c733a80e5431a69477459e5c7c52f72ad26
Muhammad Athar <athar957@gmail.com>
2023-12-25 13:01:02 +0500

    first commit

---
+++ script8.js
@@ -0,0 +1,94 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset-btn");
+let newGameBtn = document.querySelector(".new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let msgDraw = document.querySelector("#msg-draw");
+
+
+let turnO = true;  // playerX. //PlayerO
+
+const winPattern = [
+
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8]
+
+]
+
+
+const resetGame = () => {
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide")
+
+}
+boxes.forEach((box) => {
+  box.addEventListener('click', () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
+});
+
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+}
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+    box.innerText = "";
+  }
+}
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+}
+
+const checkWinner = () => {
+  for (let pattern of winPattern) {
+
+    let position1Value = boxes[pattern[0]].innerText;
+    let position2Value = boxes[pattern[1]].innerText;
+    let position3Value = boxes[pattern[2]].innerText;
+
+    if (position1Value != "" && position2Value != "" && position3Value != "") {
+      if (position1Value === position2Value && position2Value === position3Value) {
+        showWinner(position1Value);
+      }
+    }
+  }
+};
+
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+
+
+// These are used for debugging in checkWinner function
+// console.log(pattern[0], pattern[1], pattern[2],)
+// console.log(boxes[pattern[0]], boxes[pattern[1]], boxes[pattern[2]
+// console.log(pattern[0], pattern[1], pattern[2])
+// console.log(
+//   boxes[pattern[0]].innerText,
+//   boxes[pattern[1]].innerText,
+//   boxes[pattern[2]].innerText,
+// )
